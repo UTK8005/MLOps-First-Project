f183679acc1702ac34f1e3a41b18763c5ba18a50
UTK8005 <utkarsh.ujwal8005@gmail.com>
2024-07-25 11:20:14 +0100

    Ran Template. py and created necessary files

--- Template.py
+++ Template.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import logging
 
 list_of_files=[
 
@@ -27,3 +28,14 @@ list_of_files=[
     "experiment/experiments.ipynb"
 
 ]
+
+for filepath in list_of_files:
+    filepath= Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass # create an empty file
+        
